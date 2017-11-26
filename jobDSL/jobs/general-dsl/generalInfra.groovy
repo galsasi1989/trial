9761e0fc64052fcc1acac5a8efffbf273f676ff2
gal sasi <gal.sasi@hpe.com>
2017-11-27 01:42:06 +0200

    add gradle wrapper

--- jobDSL/jobs/general-dsl/generalInfra.groovy
+++ jobDSL/jobs/general-dsl/generalInfra.groovy
@@ -2,7 +2,7 @@
  * Created by gal_sasi on 26/11/2017.
  */
 
-import jobdsl.factory.infra.freestylejob.freestylejob.JobRunner
+import jobdsl.factory.infra.freestylejob.JobRunner
 
 
 new JobRunner().build(this)
